db4239917cc1b4802e74dcfd0d908eb2de7a8413
Lumomag <lucasmoraesmagalhaes@gmail.com>
2023-12-01 17:39:13 -0300

    Botão de reset

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
   createGrid();
 
-  const cells = document.querySelectorAll('.container div');
+  const cells = document.querySelectorAll(".container div");
 
-  cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = '#000';
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "#000";
     });
-});
-
+  });
 
   function createGrid() {
-    const container = document.querySelector('.container');
-    for(let i = 0; i < 256; i++){
-      const cell = document.createElement('div');
+    const container = document.querySelector(".container");
+    for (let i = 0; i < 256; i++) {
+      const cell = document.createElement("div");
       container.appendChild(cell);
     }
   }
-})
+
+  const resetButton = document.querySelector(".reset-btn");
+  resetButton.addEventListener("click", () => {
+    cells.forEach((cell) => {
+      cell.style.backgroundColor = "#FFFFFF";
+    });
+  });
+});
